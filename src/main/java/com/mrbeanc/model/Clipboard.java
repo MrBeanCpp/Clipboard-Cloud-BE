b6132af989e4953e0fa8b0e53fe8d64c34ff2bdd
mrbeanc <mrbeanc@qq.com>
2024-11-09 20:42:22 +0800

    优化Clipboard.toString输出

--- src/main/java/com/mrbeanc/model/Clipboard.java
+++ src/main/java/com/mrbeanc/model/Clipboard.java
@@ -14,15 +14,27 @@ public class Clipboard {
 
     @Override
     public String toString() {
+        int bytes = data.getBytes().length;
+        String displaySize;
+
+        // human-readable size
+        if (bytes < 1024) {
+            displaySize = String.format("%dB", bytes);
+        } else if (bytes < 1024 * 1024) {
+            displaySize = String.format("%.2fKB", bytes / 1024.0);
+        } else {
+            displaySize = String.format("%.2fMB", bytes / (1024.0 * 1024));
+        }
+
         String _data = isText ?
-                Utils.omit(data, 2) + "[Text]" :
-                Utils.omit(data, 10) + "[Image]";
+                Utils.omit(data, 2) + String.format("[Text %s]", displaySize) :
+                Utils.omit(data, 10) + String.format("[Image %s]", displaySize);
 
         return "Clipboard{" +
                 "os='" + os + '\'' +
                 ", data='" + _data + '\'' +
                 ", isText=" + isText +
-                ", time=" + Utils.toLocalTime(time) +
+                ", time=" + Utils.toLocalTime(time).toString().split("\\.")[0] +
                 '}';
     }
 }
